3ee7c065f4c5b686c531517606971c6a99850eb0
Misael E. Beltran <misaelb75@users.noreply.github.com>
2017-08-27 06:52:53 -0700

    Checkpoint 9 DOM Events: Completed

---
+++ scripts/landing.js
@@ -0,0 +1,31 @@
+var pointsArray = document.getElementsByClassName('point');
+
+var animatePoints = function(points) {
+  var revealPoint = function(pnt) {
+    points[pnt].style.opacity = 1;
+    points[pnt].style.transform = "scaleX(1) translateY(0)";
+    points[pnt].style.msTransform = "scaleX(1) translateY(0)";
+    points[pnt].style.WebkitTransform = "scaleX(1) translateY(0)";
+  }
+
+for (var i=0; i < points.length; i++){
+  revealPoint(i);
+  }
+};
+
+window.onload = function() {
+ if (window.innerHeight > 950){
+   animatePoints(pointArray);
+ }
+
+
+
+  var sellingPoints = document.getElementsByClassName('selling-points')[0];
+  var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
+
+  window.addEventListener('scroll', function(event){
+    if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
+       animatePoints(pointsArray);
+     }
+  });
+}
